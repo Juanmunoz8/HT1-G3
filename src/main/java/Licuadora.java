a4c8bbfb361a95e942a58879b6a6f71bca3029cc
Juanmunoz8 <mun231047@uvg.edu.gt>
2025-01-20 11:23:00 -0600

    Update Licuadora.java

--- src/main/java/Licuadora.java
+++ src/main/java/Licuadora.java
@@ -14,4 +14,40 @@ public class Licuadora {
     public boolean isFull () {
         return full;
     }
+
+    public void changeSpeed(boolean increase) {
+        if (increase) {
+            speed = (speed + 1) > 10 ? 0 : speed + 1;
+        }
+        else {
+            speed = (speed - 1) < 0 ? 0 : speed - 1;
+        }
+    }
+
+    public int checkSpeed() {
+        if (speed == 0) {
+            state = false;
+            return speed;
+        } else {
+            state = true;
+            return speed;
+        }
+    }
+
+    public void IsOn() {
+        if (!state) {
+            state = true;
+            System.out.println("Blender is now ON.");
+        } else {
+            System.out.println("Blender is already ON.");
+        }
+    }
+
+    public void empty() {
+        currentElements = 0;
+        full = false;
+        speed = 0;
+        state = false;
+        System.out.println("Blender is now empty.");
+    }
 }
